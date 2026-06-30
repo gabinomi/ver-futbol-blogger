033621cb30c09ac7c4a166bdf458baf8cb4f1e2c
gabinomi <165750212+gabinomi@users.noreply.github.com>
2026-06-30 17:35:45 -0300

    Update route.ts

--- api/eventos/route.ts
+++ api/eventos/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 async function fetchStreamTP() {
   try {
-    const res = await fetch('https://streamtp.sbs/eventos.json', {
+    const res = await fetch('https://streamtp.sbs/wc.json?_=1782851795160', {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
         'Referer': 'https://streamtp.sbs/',
